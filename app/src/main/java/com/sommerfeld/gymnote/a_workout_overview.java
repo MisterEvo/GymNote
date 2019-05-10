c58ebcafbdfd90f35fc2fd4beeb533c4b2db17e3
MisterEvo <hanscop@hotmail.de>
2019-05-11 01:26:45 +0200

    Completed first version of the RecyclerView

--- app/src/main/java/com/sommerfeld/gymnote/a_workout_overview.java
+++ app/src/main/java/com/sommerfeld/gymnote/a_workout_overview.java
@@ -4,52 +4,95 @@ package com.sommerfeld.gymnote;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.sommerfeld.gymnote.adapters.WorkoutRecyclerAdapter;
 import com.sommerfeld.gymnote.models.Workout;
+import com.sommerfeld.gymnote.util.VerticalSpacingItemDecorator;
 
 import java.util.ArrayList;
 
 public class a_workout_overview extends AppCompatActivity {
 
     private static final String TAG = "a_workout_overview";
+
+    //UI Components
+    private RecyclerView mRecyclerview;
+
+    //Vars
+    private ArrayList<Workout> mWorkout = new ArrayList<>();
+    private WorkoutRecyclerAdapter mWorkoutRecyclerAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.workout_overview);
 
+        mRecyclerview = findViewById(R.id.recyclerView);
 
+        //Load the test obj
+        initRecyclerView();
+        loadTestObj();
+
+
+
+
+
+    }
+
+    private void initRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerview.setLayoutManager(linearLayoutManager);
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerview.addItemDecoration(itemDecorator);
+        mWorkoutRecyclerAdapter = new WorkoutRecyclerAdapter(mWorkout);
+        mRecyclerview.setAdapter(mWorkoutRecyclerAdapter);
+    }
+
+    private void loadTestObj() {
         // Create static test obj
         //Create test vars to create the object from
-        Workout workout = new Workout();
-        String titel;
+        Workout workout;
+        String title;
+        String color;
         ArrayList<String> exercise = new ArrayList<>();
         ArrayList<Integer> repsS1 = new ArrayList<>();
         ArrayList<Integer> repsS2 = new ArrayList<>();
         ArrayList<Integer> repsS3 = new ArrayList<>();
 
         //Initialize the vars with content
-        titel = "ChestBiceps";
-        exercise.add("Benchpress");
-        exercise.add("Curls");
-        repsS1.add(10);
-        repsS1.add(9);
-        repsS2.add(8);
-        repsS2.add(7);
-        repsS3.add(6);
-        repsS3.add(5);
+        for (int i = 1; i < 100; i++) {
+            workout = new Workout();
+            title = "ChestBiceps #" + i;
+            color = "orange #" + i;
+            exercise.add("Benchpress");
+            exercise.add("Curls");
+            repsS1.add(10);
+            repsS1.add(9);
+            repsS2.add(8);
+            repsS2.add(7);
+            repsS3.add(6);
+            repsS3.add(5);
 
 
-        //Pass the vars to the obj to generate the obj
-        workout.setTitle(titel);
-        workout.setExercise(exercise);
-        workout.setRepsS1(repsS1);
-        workout.setRepsS2(repsS2);
-        workout.setRepsS3(repsS3);
+            //Pass the vars to the obj to generate the obj
+            workout.setTitle(title);
+            workout.setColor(color);
+            workout.setExercise(exercise);
+            workout.setRepsS1(repsS1);
+            workout.setRepsS2(repsS2);
+            workout.setRepsS3(repsS3);
 
-        //Print obj to log using .toString
+            //Print obj to log using .toString
 
-        Log.d(TAG, "onCreate: myWorkout" + workout.toString() );
+            // Log.d(TAG, "onCreate: myWorkout" + workout.toString() );
+
+            //Pass the generated object in the corresponding outter Array
+            mWorkout.add(workout);
+        }
+        mWorkoutRecyclerAdapter.notifyDataSetChanged();
     }
 }
 
--- app/src/main/java/com/sommerfeld/gymnote/models/Workout.java
+++ app/src/main/java/com/sommerfeld/gymnote/models/Workout.java
@@ -5,13 +5,15 @@ import java.util.ArrayList;
 public class Workout {
 
     private String title;
+    private String color;
     private ArrayList<String> exercise;
     private ArrayList<Integer> repsS1;
     private ArrayList<Integer> repsS2;
     private ArrayList<Integer> repsS3;
 
-    public Workout(String title, ArrayList<String> exercise, ArrayList<Integer> repsS1, ArrayList<Integer> repsS2, ArrayList<Integer> repsS3) {
+    public Workout(String title, String color, ArrayList<String> exercise, ArrayList<Integer> repsS1, ArrayList<Integer> repsS2, ArrayList<Integer> repsS3) {
         this.title = title;
+        this.color = color;
         this.exercise = exercise;
         this.repsS1 = repsS1;
         this.repsS2 = repsS2;
@@ -19,6 +21,14 @@ public class Workout {
     }
 
     public Workout() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public String getTitle() {
@@ -65,6 +75,7 @@ public class Workout {
     public String toString() {
         return "Workout{" +
                 "title='" + title + '\'' +
+                ", color='" + color + '\'' +
                 ", exercise=" + exercise +
                 ", repsS1=" + repsS1 +
                 ", repsS2=" + repsS2 +
