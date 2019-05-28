c361bcd47ac773c0b0cfbf494031d8aef0db514f
MisterEvo <hanscop@hotmail.de>
2019-05-28 13:11:59 +0200

    Finalized export & import Feature

--- app/src/main/java/com/sommerfeld/gymnote/MainActivity.java
+++ app/src/main/java/com/sommerfeld/gymnote/MainActivity.java
@@ -1,11 +1,15 @@
 package com.sommerfeld.gymnote;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,8 +20,16 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.sommerfeld.gymnote.models.Completed;
 import com.sommerfeld.gymnote.persistence.CompletedRepo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     TextView last_workout;
     private ArrayList<Completed> mCompleteds;
     private CompletedRepo mCompletedRepo;
+    public static final int PICKFILE_RESULT_CODE = 1;
+    private Uri fileUri;
+    private String filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,11 +97,15 @@ public class MainActivity extends AppCompatActivity {
                     mCompleteds.clear();
                 }
                 if (completeds != null) {
-                    mCompleteds.addAll(completeds);
-                    String size = String.valueOf(mCompleteds.size());
-                    total_workout.setText(size);
-                    String lastWorkout = mCompleteds.get((mCompleteds.size() - 1)).getTitle();
-                    last_workout.setText(lastWorkout);
+                    try {
+                        mCompleteds.addAll(completeds);
+                        String size = String.valueOf(mCompleteds.size());
+                        total_workout.setText(size);
+                        String lastWorkout = mCompleteds.get((mCompleteds.size() - 1)).getTitle();
+                        last_workout.setText(lastWorkout);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
@@ -102,9 +121,115 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.save_Order:
+            case R.id.menu_import:
+
+                String backup1 = Environment.getExternalStorageDirectory() + File.separator + "GymNote" + File.separator + "workoutsExport.db";
+                String backup2 = Environment.getExternalStorageDirectory() + File.separator + "GymNote" + File.separator + "workoutsExport.db-shm";
+                String backup3 = Environment.getExternalStorageDirectory() + File.separator + "GymNote" + File.separator + "workoutsExport.db-wal";
+                String backupSharedPrefs = Environment.getExternalStorageDirectory() + File.separator + "GymNote" + File.separator + "pref_fileExport.xml";
+
+                importFiles(backup1, "/databases/workouts.db");
+                importFiles(backup2, "/databases/workouts.db-shm");
+                importFiles(backup3, "/databases/workouts.db-wal");
+                importFiles(backupSharedPrefs, "/shared_prefs/pref_file.xml");
+
+                break;
+            case R.id.menu_export:
+                try {
+                    String DB_FILEPATH1 = getDataDir().toString() + "/databases/workouts.db";
+                    String DB_FILEPATH2 = getDataDir().toString() + "/databases/workouts.db-shm";
+                    String DB_FILEPATH3 = getDataDir().toString() + "/databases/workouts.db-wal";
+                    String SHAREDPREFS = getDataDir().toString() + "/shared_prefs/pref_file.xml";
+                    exportFiles(DB_FILEPATH1, "workoutsExport.db");
+                    exportFiles(DB_FILEPATH2, "workoutsExport.db-shm");
+                    exportFiles(DB_FILEPATH3, "workoutsExport.db-wal");
+                    exportFiles(SHAREDPREFS, "pref_fileExport.xml");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private boolean importFiles(String inputFile, String outputFile) {
+        InputStream in = null;
+        OutputStream out = null;
+
+        //Clear the destination file first
+        try {
+            new File(getDataDir().toString() + outputFile).delete();
+            Log.d(TAG, "importFiles: Deleted: " + outputFile);
+        } catch (Exception e) {
+            Log.e(TAG, "importFiles: " + e.getMessage());
+        }
+
+        try {
+            in = new FileInputStream(inputFile);
+            out = new FileOutputStream(getDataDir().toString() + outputFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            in = null;
+
+            out.flush();
+            out.close();
+            out = null;
+            Toast.makeText(this, "importFiles: File has been copied to: " + Environment.getExternalStorageDirectory() + File.separator + "GymNote" + outputFile, Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException fnfe1) {
+            Log.e(TAG, fnfe1.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        return true;
+    }
+
+
+    private boolean exportFiles(String inputFile, String outputFile) throws IOException {
+
+        InputStream in = null;
+        OutputStream out = null;
+
+        File folder = new File(Environment.getExternalStorageDirectory() + File.separator + "GymNote");
+        boolean success = true;
+        if (!folder.exists()) {
+            success = folder.mkdirs();
+        }
+
+        if (success) {
+            //Folder is existing
+            Log.d(TAG, "exportFiles: Folder created: " + folder.toPath());
+            try {
+                in = new FileInputStream(inputFile);
+                out = new FileOutputStream(folder.toPath() + File.separator + outputFile);
+
+                byte[] buffer = new byte[1024];
+                int read;
+                while ((read = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, read);
+                }
+                in.close();
+                in = null;
+
+                out.flush();
+                out.close();
+                out = null;
+                Log.d(TAG, "exportFiles: File has been copied to: " + folder.toPath() + File.separator + outputFile);
+            } catch (FileNotFoundException fnfe1) {
+                Log.e(TAG, fnfe1.getMessage());
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+        } else {
+            Log.d(TAG, "exportFiles: Failed to create folder: " + folder.toPath());
+        }
+
+        return true;
     }
 }
